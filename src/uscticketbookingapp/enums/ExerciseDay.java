7f7d5ae54f6dc8de10e547b394503e062f86d169
niyi <niyi@10.2.56.65>
2020-04-08 15:52:20 +0100

    Creating the ExerciseDay Enum

--- src/uscticketbookingapp/enums/ExerciseDay.java
+++ src/uscticketbookingapp/enums/ExerciseDay.java
@@ -15,6 +15,7 @@ import java.util.ArrayList;
 public enum ExerciseDay {
     SATURDAY,
     SUNDAY
+    //
 //    String dayType;
 //    ArrayList<ExerciseClass> exCourses;
 //    static final int MAX_COURSE_IN_A_DAY = 3;
